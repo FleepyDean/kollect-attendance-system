103c180797e4d978a986e3c01a7bc41a11723501
MUHAMMAD AMIN BIN JAAFAR <141222142+amin03-utm@users.noreply.github.com>
2024-04-14 02:11:03 +0800

    Change the icon and name of the title at employee side

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <title>Employee Attendance Management System</title>
+    <title>KollectLog</title>
+    <link rel="icon" href="assets/img/eam-logo.png"/>
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
